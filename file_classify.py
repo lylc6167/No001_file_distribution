7a19effc3a75944ab497db03e66c95e2b617271c
6167laptop <6167.good@163.com>
2018-04-10 16:40:37 +0800

    增加error判断，大写文件夹，，修改覆盖写为续写模式'w','a'

--- file_classify.py
+++ file_classify.py
@@ -1,6 +1,10 @@
+#测试成功，但是origin文件夹消失
+#思考后，因为renames是修改文件全名包含目录，所以源目录消失了
+#当文件与目标文件夹冲突时，会有文件留在原文件夹，则不消失
+#这其中就体现出与shutil的不同了
 import os
 
-path_old=  'F:\git\personal\\No001_file_distribution\\test\\origin\\'
+path_old=  'F:\git\personal\\No001_file_distribution\\test\\origin\\origin\\'
 path_new=  'F:\git\personal\\No001_file_distribution\\test\\target\\'
 
 #初始化目录，视情况调用,
@@ -11,10 +15,21 @@ path_new=  'F:\git\personal\\No001_file_distribution\\test\\target\\'
 result   =  os.walk(path_old)
 for path_old, w, files in result:
     pass
+#print(files)
 
-print(files)
+with open('F:\\git\\personal\\No001_file_distribution\\test\\origin\\index.txt','a') as f:
+    #覆盖写模式是w，续写模式是a
+   for fi in files:
+       fi=[''.join(fi)+'\n']
+       f.writelines(fi)
+
 
 for file in files:
     folder = file.split('-')
     folder = folder[0]+'\\'
-    os.renames(path_old+file,path_new+folder+file)
+    try:
+        os.renames(path_old+file,path_new+folder.upper()+file)#大写文件夹
+    except FileExistsError:
+        print(file+ ' is Exists')
+
+
