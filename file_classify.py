335cd2ca2c895a14cc57ea7d97a98cbc0fe80fbc
6167laptop <6167.good@163.com>
2018-04-12 00:28:45 +0800

    第一次实测，出现日文编码问题，以及部分番号错误导致的错误文件夹，例如S-cute

--- file_classify.py
+++ file_classify.py
@@ -2,11 +2,12 @@
 #思考后，因为renames是修改文件全名包含目录，所以源目录消失了
 #当文件与目标文件夹冲突时，会有文件留在原文件夹，则不消失
 #这其中就体现出与shutil的不同了
+#文件转码有问题，部分错误命名有问题如S-cute
 import os
 
-path_old=  'F:\git\personal\\No001_file_distribution\\test\\origin\\origin\\'
-path_new=  'F:\git\personal\\No001_file_distribution\\test\\target\\'
-path_index='F:\\git\\personal\\No001_file_distribution\\test\\origin\\index.txt'
+path_old=  'H:\\seed\\2.番号\\封面中转\\暂存\\'
+path_new=  'H:\\seed\\2.番号\\'
+path_index='H:\\seed\\2.番号\\封面中转\\index.txt'
 #初始化目录，视情况调用,
 #def initial(pat):
 #    pat=pat.strip().rstrip('\\')
@@ -21,7 +22,10 @@ with open(path_index,'a') as f:
     #覆盖写模式是w，续写模式是a
    for fi in files:
        fi=[''.join(fi)+'\n']
-       f.writelines(fi)
+       try:
+           f.writelines(fi)
+       except UnicodeEncodeError:
+           f.writelines('Encoding Error!!!!!!!!!')
 
 
 for file in files:
